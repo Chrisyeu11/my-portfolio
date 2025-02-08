168b828819dfc623fce45262972df9a4caf2999a
Chrisyeu11 <chrisyeu11@gmail.com>
2025-02-08 15:07:22 -0600

    Fixed JSX escaping issue in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="p-4 bg-gray-800 text-white fixed w-full top-0">
       <div className="container mx-auto flex justify-between">
-        <h1 className="text-xl font-bold">Chris' Portfolio</h1>
+      <h1 className="text-xl font-bold">Chris&apos; Portfolio</h1>
         <div className="space-x-4">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
